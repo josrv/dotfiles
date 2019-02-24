07313812a0284a66d336660f3fee071eecad46ed
josrv <josrv@protonmail.com>
2019-02-24 16:20:49 +0300

    Updates

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,4 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar0 &
+# Launch polybar on all available monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bar0 &
+done
