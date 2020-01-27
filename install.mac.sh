2aba203b6176aac166cb0c03a95d8c063791e2a0
suive <is@suive.name>
2020-01-27 22:33:54 +0300

    Start writing install script

--- install.mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# TODO: DSL
-
-CURRENT_DIR=`pwd`
-ln -s "$CURRENT_DIR/config/emacs/config.org" "$HOME/.config/emacs/config.org"
-ln -s "$CURRENT_DIR/config/emacs/init.el" "$HOME/.emacs.d/init.el"
